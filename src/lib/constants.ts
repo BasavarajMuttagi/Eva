dba0d01cf16b49c113443d1a39ea70e0a0f68c2d
BasavarajMuttagi <basavaraj2770@gmail.com>
2026-03-09 23:56:45 +0530

    fix: correct API_BASE_URL reference in constants to use apiBaseUrl

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 import Constants from "expo-constants";
-export const API_BASE_URL = Constants.expoConfig?.extra?.apiUrl as string;
+export const API_BASE_URL = Constants.expoConfig?.extra?.apiBaseUrl as string;
